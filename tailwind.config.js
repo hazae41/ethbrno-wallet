04abd508443b4780a48453d5619fbfd40cb7d2f3
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-11-12 11:23:00 +0100

    tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,10 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  darkMode: "class",
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './src/**/*.{js,ts,jsx,tsx}'
+  ],
   theme: {
     extend: {},
   },
