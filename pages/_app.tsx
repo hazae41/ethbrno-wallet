5447204db0e76b0078121809aca42179c21b3073
Haz Æ 41 <4405263+hazae41@users.noreply.github.com>
2022-11-12 12:05:37 +0100

    wallet width + forge

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,11 @@
+import { XSWR } from "@hazae41/xswr"
 import type { AppProps } from 'next/app'
 import '../styles/globals.css'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <div className="w-[337.50px] h-[600px]">
+    <XSWR.CoreProvider>
+      <Component {...pageProps} />
+    </XSWR.CoreProvider>
+  </div>
 }
